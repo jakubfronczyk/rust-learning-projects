0baa48306ad955df389bd4bb91390323d74411cc
Jakub Fronczyk <jakubfronczyk@gmail.com>
2024-04-22 21:11:32 +0200

    refactor

--- factorial-calculation/src/main.rs
+++ factorial-calculation/src/main.rs
@@ -1,24 +1,32 @@
 use std::io;
 
 fn main() {
-    println!("Hello, please give an input: ");
-
-    let mut input_string = String::new();
-
-    io::stdin().read_line(&mut input_string).unwrap();
-
-    let trimmed_input = input_string.trim();
-
-    // refactor following rust best practices
-
-    let parsed_to_number: u32 = trimmed_input.parse().expect("Not a valid number");
+    let parsed_to_number: u32 = get_user_input();
 
     let results = factorial_calculation(parsed_to_number);
 
     println!("");
-    println!("{}! = {}", parsed_to_number, results);
+    println!("{}!= {}", parsed_to_number, results);
+}
 
-    // 4! = 4 x 3 x 2 x 1 = 24
+fn get_user_input() -> u32 {
+    loop {
+        println!("Hello, please give an input: ");
+
+        let mut input_string = String::new();
+
+        io::stdin().read_line(&mut input_string).unwrap();
+
+        let trimmed_input = input_string.trim();
+
+        match trimmed_input.parse::<u32>() {
+            Ok(i) => return i,
+            Err(_) => {
+                println!("Not a valid number. Please try again.");
+                continue;
+            }
+        }
+    }
 }
 
 fn factorial_calculation(n: u32) -> u32 {
