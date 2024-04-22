56f066ed61099208d9fce67fbdb1241ab9718988
Jakub Fronczyk <jakubfronczyk@gmail.com>
2024-04-22 22:00:43 +0200

    update currency-converter, need to refactor

--- currency-converter/src/main.rs
+++ currency-converter/src/main.rs
@@ -1,22 +1,31 @@
 use std::io::{self};
 
+// reactor
 fn main() {
-    loop {
-        println!("Hello, this is currency converter!");
+    println!("Hello, this is currency converter!");
+    let mut amount: f32;
+    let mut currency_choose: i32;
 
-        let amount = loop {
-            println!("Please enter the amount in EUR you want to convert:");
-            let mut amount = String::new();
-            io::stdin().read_line(&mut amount).unwrap();
-            match amount.trim().parse::<f32>() {
-                Ok(num) => break num,
+    loop {
+        loop {
+            println!("Enter the amount in USD: ");
+            let mut user_input = String::new();
+            io::stdin().read_line(&mut user_input).unwrap();
+
+            // there i need second loop for checking type
+            match user_input.trim().parse::<f32>() {
+                Ok(num) => {
+                    amount = num;
+                    break;
+                }
                 Err(_) => {
                     println!("Please enter a valid number.");
+                    continue;
                 }
             }
-        };
+        }
 
-        let input = loop {
+        loop {
             println!("Please choose the currency you want to convert to:");
             println!("1: PLN");
             println!("2: USD");
@@ -25,34 +34,40 @@ fn main() {
 
             let mut input = String::new();
             io::stdin().read_line(&mut input).unwrap();
+
             match input.trim().parse::<i32>() {
-                Ok(num) => break num,
+                Ok(num) => {
+                    currency_choose = num;
+                    break;
+                }
                 Err(_) => {
                     println!("Please enter a valid number.");
+                    continue;
                 }
             }
-        };
+        }
 
-        match input {
+        match currency_choose {
             1 => {
-                let converted = amount * 4.54; // Assume 1 EUR = 4.54 PLN
-                println!("{} EUR is {} PLN", amount, converted);
+                let converted = amount * 4.54;
+                println!("{} EUR converted to {} PLN", amount, converted);
             }
             2 => {
-                let converted = amount * 1.13; // Assume 1 EUR = 1.13 USD
-                println!("{} EUR is {} USD", amount, converted);
+                let converted = amount * 1.13;
+                println!("{} EUR converted to {} USD", amount, converted);
             }
             3 => {
-                let converted = amount * 129.53; // Assume 1 EUR = 129.53 YEN
-                println!("{} EUR is {} YEN", amount, converted);
+                let converted = amount * 129.53;
+                println!("{} EUR converted to {} YEN", amount, converted);
             }
-            _ => break,
+            _ => println!("Exit"),
         }
 
         let continue_conversion = loop {
             println!("Do you want to convert another currency? (yes/no)");
             let mut answer = String::new();
             io::stdin().read_line(&mut answer).unwrap();
+
             match answer.trim().to_lowercase().as_str() {
                 "yes" => break true,
                 "no" => break false,
