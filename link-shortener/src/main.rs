5ea4d3b91a4afb3701603af740509f087c8aa69e
Jakub Fronczyk <jakubfronczyk@gmail.com>
2024-05-19 22:10:04 +0200

    check if redirect route working correctly

--- link-shortener/src/main.rs
+++ link-shortener/src/main.rs
@@ -1,35 +1,68 @@
-mod routes;
-
 use std::error::Error;
 
 use axum::{routing::get, Router};
-use axum_prometheus::{lifecycle::layer, PrometheusMetricLayer};
+use axum_prometheus::PrometheusMetricLayer;
+use dotenvy::dotenv;
 use routes::health;
+use sqlx::postgres::PgPoolOptions;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+use crate::routes::redirect;
+
+mod routes;
+mod utils;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // Load environment variables from .env file if it exists
+    dotenv().ok();
+
+    // Setup tracing
     tracing_subscriber::registry()
+        // Set the filter level based on the environment variable or default to "link_shortener=debug"
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "link_shortener=debug".into()),
         )
+        // Add the fmt layer for formatting logs
         .with(tracing_subscriber::fmt::layer())
+        // Initialize the tracing subscriber
         .init();
 
+    // Read the db url
+    let db_url =
+        std::env::var("DATABASE_URL").expect("DATABASE_URL is a required environment variables");
+
+    // Create a connection pool
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&db_url)
+        .await?;
+
+    // Create a new instance of PrometheusMetricLayer and get a handle to metrics
     let (prometheus_layer, metrics_handle) = PrometheusMetricLayer::pair();
 
+    // Create the Axum application
     let app = Router::new()
+        .route("/:id", get(redirect))
+        // Define a route for "/metrics" endpoint to serve Prometheus metrics
         .route("/metrics", get(|| async move { metrics_handle.render() }))
+        // Define a route for "/health" endpoint using the health function imported from routes module
         .route("/health", get(health))
+        // Add TraceLayer for HTTP tracing
         .layer(TraceLayer::new_for_http())
-        .layer(prometheus_layer);
+        // Add PrometheusMetricLayer for metrics
+        .layer(prometheus_layer)
+        //
+        .with_state(db);
 
+    // Bind a TCP listener to address "0.0.0.0:3000"
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .expect("Could not initialize TcpListener");
 
+    // Print debug message indicating the address the server is listening on
     tracing::debug!(
         "listening on {}",
         listener
@@ -37,8 +70,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .expect("Could not convert listener address to local address")
     );
 
+    // Start the Axum server to serve the application
     axum::serve(listener, app)
         .await
         .expect("Could not successfully create server");
+
+    // Return Ok(()) to indicate successful execution of the main function
     Ok(())
 }
--- link-shortener/src/routes.rs
+++ link-shortener/src/routes.rs
@@ -1,5 +1,58 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::body::Body;
+use axum::extract::{Path, State};
+use axum::http::{HeaderMap, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use sqlx::error::ErrorKind;
+use sqlx::{Error, PgPool};
+
+use crate::utils::internal_error;
+
+const DEFAULT_CACHE_CONTROL_HEADER_VALUE: &str =
+    "public, max-age=300, s-maxage=300, stale-while-revalidate=300, stale-if-error=300";
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Link {
+    pub id: String,
+    pub target_url: String,
+}
 
 pub async fn health() -> impl IntoResponse {
     (StatusCode::OK, "Services is healthy")
+}
+
+pub async fn redirect(
+    State(pool): State<PgPool>,
+    Path(requested_link): Path<String>,
+) -> Result<Response, (StatusCode, String)> {
+    let select_timeout = tokio::time::Duration::from_millis(300);
+
+    let link = tokio::time::timeout(
+        select_timeout,
+        sqlx::query_as!(
+            Link,
+            "select id, target_url from links where id = $1",
+            requested_link
+        )
+        .fetch_optional(&pool),
+    )
+    .await
+    .map_err(internal_error)?
+    .map_err(internal_error)?
+    .ok_or_else(|| "Not found".to_string())
+    .map_err(|err| (StatusCode::NOT_FOUND, err))?;
+
+    tracing::debug!(
+        "Redirecting link id {} to {}",
+        requested_link,
+        link.target_url
+    );
+
+    Ok(Response::builder()
+        .status(StatusCode::TEMPORARY_REDIRECT)
+        .header("Location", link.target_url)
+        .header("Cache-Control", DEFAULT_CACHE_CONTROL_HEADER_VALUE)
+        .body(Body::empty())
+        .expect("This response should always be constructable"))
 }
